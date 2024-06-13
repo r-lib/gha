cc8d932a0e1a2ee9c93521698eb7daa48d560b80
Hadley Wickham <h.wickham@gmail.com>
2024-06-13 16:56:40 -0500

    Try a global handler for warnings

--- .github/examples.R
+++ .github/examples.R
@@ -1,4 +1,5 @@
 library(gha)
+library(rlang)
 
 gha_debug("Debugging")
 gha_notice("Notice me!")
@@ -7,3 +8,17 @@ gha_error("DANGER WILL ROBINSON!")
 
 gha_summary("# Heading")
 gha_summary(c(" ", "* A", "* B"))
+
+
+globalCallingHandlers(warning = function(cnd) {
+  call <- conditionCall(cnd)
+  message <- conditionMessage(cnd)
+  gha_warning("{call}: {message}")
+})
+
+f <- function() g()
+g <- function() h()
+h <- function() {
+  warning("This is a warning!")
+}
+f()
